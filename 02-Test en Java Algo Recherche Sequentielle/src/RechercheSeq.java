e87985d253985e1af241730ca4d6ce84bd2e30d2
Shoyur <100440995+Shoyur@users.noreply.github.com>
2022-09-20 07:57:53 -0400

    ...

---
+++ 02-Test en Java Algo Recherche Sequentielle/src/RechercheSeq.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class RechercheSeq {
+
+	
+	public static void methode1() {
+		int i = 1;
+		int j = 0;
+		try {
+		i = i / j;
+		} catch(ArithmeticException e) {
+			e.printStackTrace(); // affiche la trace d'ex�cution
+			System.out.println("Exception attrapée...");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+			methode1();
+			System.exit(0);}
+
+}
